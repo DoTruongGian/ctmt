94831274514889820efdf07750d68dde9e5dc0c0
DoTruongGian <truonggiangdo27112k3@gmail.com>
2024-10-02 19:26:28 +0700

    milestone1_update

--- milestone1_test/simulation/qsim/work/milestone1_test_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity milestone1_test_vlg_vec_tst is
-end milestone1_test_vlg_vec_tst;
--- milestone1_test/work/milestone1_test_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity milestone1_test_tb is
-end milestone1_test_tb;
